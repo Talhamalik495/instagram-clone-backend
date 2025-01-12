100a374d0fe9d85c9abbf0e288104278d28570e6
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2025-01-13 01:00:45 +0500

    pakage add

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import express from "express"
- let app=express()
-
-
- app.listen()
+import express from "express";
+let app = express();
+import "dotenv/config";
+import cors from "cors";
+app.use(express.json());
+app.use(cors("*"));
+app.get("/", (req, res) => {
+  res.send("Get request called");
+});
+app.listen(process.env.PORT, () => {
+  console.log("server is runing on port " + process.env.PORT);
+});
