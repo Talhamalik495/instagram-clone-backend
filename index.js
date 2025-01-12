93ad001fe290f9e24c67ace875c386275521368f
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2025-01-13 01:23:00 +0500

    Database conncet

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ import express from "express";
 let app = express();
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
 app.use(express.json());
 app.use(cors("*"));
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 app.get("/", (req, res) => {
   res.send("Get request called");
 });
