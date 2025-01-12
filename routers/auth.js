22b9d2cb9619714e99285506bc57b208c8111c9e
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2025-01-13 01:58:03 +0500

    register api 50% commplete

--- routers/auth.js
+++ routers/auth.js
@@ -1,8 +1,28 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { register } from "./models/register";
 let router = express.Router();
 
 router.post("/register", (req, res) => {
   let obj = req.body;
-  
+  let dbUserCheck = register.findOne({ email: obj.email });
+  if (dbUserCheck) {
+    res.send(400).json({
+      error: true,
+      meassage: "User already registered",
+      user: null,
+    });
+    let saltRounds = 12;
+    let hashedPassword = bcrypt.hash(obj.password, saltRounds);
+    obj.password = hashedPassword;
+
+    let newUser = new register(obj);
+    newUser = newUser.save();
+    res.send(200).json({
+      error: false,
+      meassage: "User registerd successfully",
+      user: newUser,
+    });
+  }
   res.send("register api creacte");
 });
